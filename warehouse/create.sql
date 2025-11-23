75c2f19e51b0aadd3445e06d3f61e0170246b813
Bartosz <bartilyska@gmail.com>
2025-11-23 16:54:28 +0000

    dodawanie bardziej sensownych danych

--- warehouse/create.sql
+++ warehouse/create.sql
@@ -1,9 +1,9 @@
-
-CREATE DATABASE hurtownia;
+-- wczesniej CREATE DATABASE hurtownia;
 USE hurtownia;
 
 DROP TABLE IF EXISTS Zdarzenie_na_trasie;
 DROP TABLE IF EXISTS Odcinek_kursu;
+DROP TABLE IF EXISTS Kolejnosc_odcinkow;
 DROP TABLE IF EXISTS Junk_zdarzenie;
 DROP TABLE IF EXISTS Junk_odcinek_kursu;
 DROP TABLE IF EXISTS Czas;
@@ -19,18 +19,19 @@ CREATE TABLE Pociag (
     id INT IDENTITY(1,1) PRIMARY KEY,
     nazwa VARCHAR(20) NOT NULL,
     typ_pociagu VARCHAR(30) NOT NULL,
-    operator VARCHAR(40) NOT NULL
+    przewoznik VARCHAR(40) NOT NULL
 );
 
 CREATE TABLE Maszynista (
     id INT IDENTITY(1,1) PRIMARY KEY,
     imie VARCHAR(30) NOT NULL,
     nazwisko VARCHAR(30) NOT NULL,
-    plec VARCHAR(10) NOT NULL,
+    plec VARCHAR(10) NOT NULL CHECK (plec IN ('man', 'woman')),
     kategoria_wiekowa VARCHAR(30) NOT NULL,
     doswiadczenie_pracy VARCHAR(30) NOT NULL,
     pesel CHAR(11) NOT NULL,
-    czy_aktualne BIT NOT NULL
+    czy_aktualne BIT NOT NULL,
+    CONSTRAINT chk_pesel_format CHECK (pesel LIKE '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]')
 );
 
 CREATE TABLE Stacja (
@@ -64,9 +65,10 @@ CREATE TABLE Data (
     id INT IDENTITY(1,1) PRIMARY KEY,
     rok INT NOT NULL,
     miesiac VARCHAR(20) NOT NULL,
-    dzien INT NOT NULL,
+    numer_miesiaca INT NOT NULL,
     pora_roku VARCHAR(20) NOT NULL,
-    dzien_tygodnia VARCHAR(20) NOT NULL
+    dzien_tygodnia VARCHAR(20) NOT NULL,
+    dzien INT NOT NULL
 );
 
 CREATE TABLE Czas (
@@ -86,153 +88,198 @@ CREATE TABLE Junk_zdarzenie (
     czy_interwencja_sluzb BIT NOT NULL
 );
 
+CREATE TABLE Kolejnosc_odcinkow (
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    numer_etapu INT NOT NULL UNIQUE,
+);
+
 CREATE TABLE Odcinek_kursu (
     id INT IDENTITY(1,1) PRIMARY KEY,
-    id_kurs INT NOT NULL,
-    id_pociag INT NOT NULL,
-    id_stacja_wyjazdowa INT NOT NULL,
-    id_stacja_wjazdowa INT NOT NULL,
-    id_maszynista INT NOT NULL,
-    id_planowa_data_przyjazdu INT NOT NULL,
-    id_planowa_data_odjazdu INT NOT NULL,
-    id_planowy_czas_przyjazdu INT NOT NULL,
-    id_planowy_czas_odjazdu INT NOT NULL,
-    id_junk INT NOT NULL,
-    numer_etapu_kursu INT,
+    id_kurs INT NOT NULL REFERENCES Kurs(id),
+    id_pociag INT NOT NULL REFERENCES Pociag(id),
+    id_stacja_wyjazdowa INT NOT NULL REFERENCES Stacja(id),
+    id_stacja_wjazdowa INT NOT NULL REFERENCES Stacja(id),
+    id_maszynista INT NOT NULL REFERENCES Maszynista(id),
+    id_planowa_data_przyjazdu INT NOT NULL REFERENCES Data(id),
+    id_planowa_data_odjazdu INT NOT NULL REFERENCES Data(id),
+    id_planowy_czas_przyjazdu INT NOT NULL REFERENCES Czas(id),
+    id_planowy_czas_odjazdu INT NOT NULL REFERENCES Czas(id),
+    id_junk INT NOT NULL REFERENCES Junk_odcinek_kursu(id),
+    id_kolejnosc_odcinkow INT NOT NULL REFERENCES Kolejnosc_odcinkow(id),
     temperatura FLOAT,
     roznica_czasu INT,
-    ilosc_opadow INT,
-    CONSTRAINT FK_Odc_Kurs FOREIGN KEY (id_kurs) REFERENCES Kurs(id),
-    CONSTRAINT FK_Odc_Poc FOREIGN KEY (id_pociag) REFERENCES Pociag(id),
-    CONSTRAINT FK_Odc_StW FOREIGN KEY (id_stacja_wyjazdowa) REFERENCES Stacja(id),
-    CONSTRAINT FK_Odc_StJ FOREIGN KEY (id_stacja_wjazdowa) REFERENCES Stacja(id),
-    CONSTRAINT FK_Odc_Mas FOREIGN KEY (id_maszynista) REFERENCES Maszynista(id),
-    CONSTRAINT FK_Odc_DaP FOREIGN KEY (id_planowa_data_przyjazdu) REFERENCES Data(id),
-    CONSTRAINT FK_Odc_DaO FOREIGN KEY (id_planowa_data_odjazdu) REFERENCES Data(id),
-    CONSTRAINT FK_Odc_CzP FOREIGN KEY (id_planowy_czas_przyjazdu) REFERENCES Czas(id),
-    CONSTRAINT FK_Odc_CzO FOREIGN KEY (id_planowy_czas_odjazdu) REFERENCES Czas(id),
-    CONSTRAINT FK_Odc_Junk FOREIGN KEY (id_junk) REFERENCES Junk_odcinek_kursu(id)
+    ilosc_opadow INT
 );
 
 CREATE TABLE Zdarzenie_na_trasie (
-    id_odcinek_kursu INT NOT NULL,
-    id_przejazd INT NULL,
-    id_zdarzenie INT NOT NULL,
-    id_junk INT NOT NULL,
+    id_odcinek_kursu INT NOT NULL REFERENCES Odcinek_kursu(id),
+    id_przejazd INT NOT NULL REFERENCES Przejazd(id),
+    id_zdarzenie INT NOT NULL REFERENCES Zdarzenie(id),
+    id_junk INT NOT NULL REFERENCES Junk_zdarzenie(id),
+    id_data_zdarzenia INT NOT NULL REFERENCES Data(id),
+    id_czas_zdarzenia INT NOT NULL REFERENCES Czas(id),
     koszt_naprawy FLOAT,
     predkosc INT,
     wywolane_opoznienie INT,
     liczba_rannych INT,
     liczba_zgonow INT,
-    FOREIGN KEY (id_odcinek_kursu) REFERENCES Odcinek_kursu(id),
-    FOREIGN KEY (id_przejazd) REFERENCES Przejazd(id),
-    FOREIGN KEY (id_zdarzenie) REFERENCES Zdarzenie(id),
-    FOREIGN KEY (id_junk) REFERENCES Junk_zdarzenie(id)
+    PRIMARY KEY (id_odcinek_kursu, id_przejazd, id_zdarzenie, id_junk, id_data_zdarzenia, id_czas_zdarzenia)
 );
 
--- Pociag data
-INSERT INTO Pociag (nazwa, typ_pociagu, operator) VALUES
-('Express 100', 'Elektryczny', 'PKP Intercity'),
-('Regional 200', 'Spalinowy', 'PKP Przewozy Regionalne'),
-('Cargo 300', 'Towarowy', 'PKP Cargo'),
-('Pendolino 400', 'Wysokospeed', 'PKP Intercity'),
-('Local 500', 'Lokalny', 'PKP Przewozy Regionalne');
+INSERT INTO Pociag (nazwa, typ_pociagu, przewoznik) VALUES
+('KM 6153', 'passenger', 'Koleje Mazowieckie'),
+('IC 7624', 'passenger', 'PKP Intercity'),
+('KS 6176', 'passenger', 'Koleje Śląskie'),
+('IC 8234', 'passenger', 'PKP Intercity'),
+('KM 4536', 'passenger', 'Koleje Mazowieckie'),
+('KD 6552', 'passenger', 'Koleje Dolnośląskie'),
+('PR 51652', 'passenger', 'POLREGIO'),
+('DB 8384', 'cargo', 'DB Cargo Polska'),
+('IC 7680', 'passenger', 'PKP Intercity'),
+('IC 6555', 'passenger', 'PKP Intercity'),
+('KM 2624', 'passenger', 'Koleje Mazowieckie'),
+('PR 60889', 'passenger', 'POLREGIO'),
+('ET 2969', 'cargo', 'PKP Cargo'),
+('PR 54401', 'passenger', 'POLREGIO'),
+('ET 3066', 'cargo', 'PKP Cargo');
 
--- Maszynista data
 INSERT INTO Maszynista (imie, nazwisko, plec, kategoria_wiekowa, doswiadczenie_pracy, pesel, czy_aktualne) VALUES
-('Jan', 'Kowalski', 'M', '35-45', '10-15 lat', '85010112345', 1),
-('Anna', 'Nowak', 'K', '25-35', '5-10 lat', '90020223456', 1),
-('Piotr', 'Wiśniewski', 'M', '45-55', '15-20 lat', '78030334567', 1),
-('Maria', 'Wójcik', 'K', '35-45', '10-15 lat', '85040445678', 1),
-('Tomasz', 'Kozłowski', 'M', '25-35', '5-10 lat', '92050556789', 1);
+('Wiktor', 'Kuran', 'man', '25-35', '10-15 lat', '99010112345', 1),
+('Kornel', 'Buchalik', 'man', '25-35', '1-5 lat', '99010112346', 1),
+('Jacek', 'Pastuszko', 'man', '25-35', '1-5 lat', '99010112347', 1),
+('Adrian', 'Amanowicz', 'man', '25-35', '1-5 lat', '99010112348', 1),
+('Emil', 'Szachniewicz', 'man', '25-35', '10-15 lat', '99010112349', 1),
+('Dominik', 'Brojek', 'man', '25-35', '1-5 lat', '99010112350', 1),
+('Albert', 'Korbut', 'man', '55-65', '30+ lat', '99010112351', 1),
+('Marta', 'Skoneczna', 'woman', '60+', '30+ lat', '99010112352', 1),
+('Maurycy', 'Padło', 'man', '35-45', '25-30 lat', '99010112353', 1),
+('Ksawery', 'Pachnik', 'man', '35-45', '15-20 lat', '99010112354', 1);
 
--- Stacja data
 INSERT INTO Stacja (nazwa, miasto) VALUES
-('Warszawa Centralna', 'Warszawa'),
-('Kraków Główny', 'Kraków'),
-('Gdańsk Główny', 'Gdańsk'),
-('Wrocław Główny', 'Wrocław'),
-('Poznań Główny', 'Poznań'),
-('Łódź Fabryczna', 'Łódź'),
-('Katowice', 'Katowice'),
-('Szczecin Główny', 'Szczecin');
+('Stacja Marki', 'Marki'),
+('Stacja Elbląg', 'Elbląg'),
+('Stacja Szczecin', 'Szczecin'),
+('Stacja Koło', 'Koło'),
+('Stacja Tarnowskie Góry', 'Tarnowskie Góry'),
+('Stacja Tomaszów Mazowiecki', 'Tomaszów Mazowiecki'),
+('Stacja Suwałki', 'Suwałki'),
+('Stacja Chorzów', 'Chorzów'),
+('Stacja Świebodzice', 'Świebodzice'),
+('Stacja Dąbrowa Górnicza', 'Dąbrowa Górnicza');
 
--- Kurs data
 INSERT INTO Kurs (nazwa_trasy, kategoria_opoznienia) VALUES
-('Warszawa-Kraków', 'Brak opóźnienia'),
-('Gdańsk-Warszawa', 'Małe opóźnienie'),
-('Wrocław-Poznań', 'Średnie opóźnienie'),
-('Katowice-Warszawa', 'Duże opóźnienie'),
-('Szczecin-Poznań', 'Brak opóźnienia');
+('Linia 22-93', 'Brak opóźnienia'),
+('Linia 48-175', 'Małe opóźnienie'),
+('Linia 34-80', 'Brak opóźnienia'),
+('Linia 174-93', 'Średnie opóźnienie'),
+('Linia 31-62', 'Duże opóźnienie'),
+('Linia 116-100', 'Średnie opóźnienie'),
+('Linia 33-149', 'Średnie opóźnienie'),
+('Linia 70-18', 'Małe opóźnienie'),
+('Linia 1-196', 'Duże opóźnienie'),
+('Linia 43-22', 'Średnie opóźnienie');
 
--- Przejazd data
 INSERT INTO Przejazd (czy_rogatki, czy_sygnalizacja_swietlna, czy_oswietlony, dopuszczalna_predkosc) VALUES
-(1, 1, 1, '100 km/h'),
-(1, 0, 1, '80 km/h'),
-(0, 1, 0, '60 km/h'),
-(1, 1, 0, '90 km/h'),
-(0, 0, 1, '70 km/h');
+(1, 1, 1, '35'),
+(0, 0, 0, '61'),
+(0, 0, 0, '82'),
+(0, 1, 1, '52'),
+(0, 0, 0, '72'),
+(0, 0, 1, '64'),
+(1, 0, 1, '58'),
+(1, 1, 1, '59'),
+(0, 0, 0, '54'),
+(0, 0, 0, '38');
 
--- Zdarzenie data
 INSERT INTO Zdarzenie (typ_zdarzenia, kategoria, skala_niebezpieczenstwa) VALUES
-('Kolizja', 'Wypadek', 'Wysoka'),
-('Awaria', 'Techniczna', 'Średnia'),
-('Opóźnienie', 'Organizacyjna', 'Niska'),
-('Przejazd', 'Bezpieczeństwo', 'Średnia'),
-('Sygnalizacja', 'Techniczna', 'Niska');
+('wypadek', 'potrącenie pieszego', '9'),
+('wypadek', 'zderzenie z samochodem', '8'),
+('wypadek', 'wykolejenie', '10'),
+('wypadek', 'zderzenie z innym pociągiem', '10'),
+('incydent', 'opóźnienie organizacyjne', '4'),
+('incydent', 'przekroczenie limitu prędkości', '5'),
+('incydent', 'problem z pasażerem', '3'),
+('awaria', 'usterka hamulców', '7'),
+('awaria', 'usterka sygnalizacji', '6'),
+('awaria', 'awaria lokomotywy', '7'),
+('zdarzenie techniczne', 'planowy postój', '2'),
+('zdarzenie techniczne', 'test systemu', '2'),
+('zdarzenie techniczne', 'brak maszynisty', '3');
 
--- Data dimension
-INSERT INTO Data (rok, miesiac, dzien, pora_roku, dzien_tygodnia) VALUES
-(2024, 'Styczeń', 15, 'Zima', 'Poniedziałek'),
-(2024, 'Luty', 20, 'Zima', 'Wtorek'),
-(2024, 'Marzec', 10, 'Wiosna', 'Środa'),
-(2024, 'Kwiecień', 5, 'Wiosna', 'Czwartek'),
-(2024, 'Maj', 12, 'Wiosna', 'Piątek'),
-(2024, 'Czerwiec', 18, 'Lato', 'Sobota'),
-(2024, 'Lipiec', 25, 'Lato', 'Niedziela'),
-(2024, 'Sierpień', 8, 'Lato', 'Poniedziałek');
+INSERT INTO Data (rok, miesiac, numer_miesiaca, dzien, pora_roku, dzien_tygodnia) VALUES
+(2023, 'Styczeń', 1, 1, 'Zima', 'Niedziela'),
+(2023, 'Styczeń', 1, 15, 'Zima', 'Niedziela'),
+(2023, 'Luty', 2, 20, 'Zima', 'Poniedziałek'),
+(2023, 'Marzec', 3, 10, 'Wiosna', 'Piątek'),
+(2023, 'Kwiecień', 4, 5, 'Wiosna', 'Środa'),
+(2023, 'Maj', 5, 12, 'Wiosna', 'Piątek'),
+(2023, 'Czerwiec', 6, 18, 'Lato', 'Niedziela'),
+(2023, 'Lipiec', 7, 25, 'Lato', 'Wtorek'),
+(2023, 'Sierpień', 8, 8, 'Lato', 'Wtorek'),
+(2023, 'Wrzesień', 9, 15, 'Jesień', 'Piątek'),
+(2023, 'Październik', 10, 30, 'Jesień', 'Poniedziałek'),
+(2023, 'Listopad', 11, 20, 'Jesień', 'Poniedziałek'),
+(2023, 'Grudzień', 12, 25, 'Zima', 'Poniedziałek'),
+(2024, 'Styczeń', 1, 10, 'Zima', 'Środa'),
+(2024, 'Czerwiec', 6, 5, 'Lato', 'Środa');
 
--- Czas dimension
 INSERT INTO Czas (godzina, minuta, pora_dnia) VALUES
-(6, 30, 'Rano'),
-(9, 15, 'Rano'),
+(6, 0, 'Rano'),
+(7, 30, 'Rano'),
+(9, 0, 'Rano'),
+(10, 30, 'Rano'),
 (12, 0, 'Południe'),
-(15, 45, 'Popołudnie'),
-(18, 20, 'Wieczór'),
-(21, 10, 'Noc'),
-(3, 5, 'Noc'),
-(8, 50, 'Rano');
+(13, 30, 'Popołudnie'),
+(15, 0, 'Popołudnie'),
+(16, 30, 'Popołudnie'),
+(11, 15, 'Rano'),
+(14, 0, 'Popołudnie'),
+(15, 30, 'Popołudnie'),
+(17, 0, 'Popołudnie'),
+(18, 30, 'Wieczór'),
+(20, 0, 'Wieczór'),
+(21, 30, 'Wieczór'),
+(22, 45, 'Noc'),
+(22, 30, 'Noc');
 
--- Junk_odcinek_kursu data
 INSERT INTO Junk_odcinek_kursu (typ_opadow) VALUES
-('Brak opadów'),
-('Deszcz lekki'),
-('Deszcz średni'),
-('Śnieg'),
-('Mgła');
+('brak'),
+('deszcz'),
+('snieg'),
+('grad'),
+('deszcz');
 
--- Junk_zdarzenie data
 INSERT INTO Junk_zdarzenie (czy_interwencja_sluzb) VALUES
 (1),
-(0),
+(0);
+
+INSERT INTO Kolejnosc_odcinkow (numer_etapu) VALUES
 (1),
-(0),
-(1);
+(2),
+(3),
+(4),
+(5),
+(6),
+(7),
+(8),
+(9),
+(10);
 
--- Odcinek_kursu fact data
-INSERT INTO Odcinek_kursu (id_kurs, id_pociag, id_stacja_wyjazdowa, id_stacja_wjazdowa, id_maszynista, id_planowa_data_przyjazdu, id_planowa_data_odjazdu, id_planowy_czas_przyjazdu, id_planowy_czas_odjazdu, id_junk, numer_etapu_kursu, temperatura, roznica_czasu, ilosc_opadow) VALUES
-(1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 5.5, 120, 0),
-(1, 1, 2, 7, 1, 2, 2, 2, 3, 2, 2, 8.2, 95, 2),
-(2, 2, 3, 1, 2, 3, 3, 3, 4, 3, 1, 12.8, 180, 5),
-(3, 3, 4, 5, 3, 4, 4, 4, 5, 1, 1, -2.1, 75, 0),
-(4, 4, 7, 1, 4, 5, 5, 5, 6, 4, 1, 15.3, 200, 8),
-(5, 5, 8, 5, 5, 6, 6, 6, 7, 5, 1, 22.7, 150, 12);
+INSERT INTO Odcinek_kursu (id_kurs, id_pociag, id_stacja_wyjazdowa, id_stacja_wjazdowa, id_maszynista, id_planowa_data_przyjazdu, id_planowa_data_odjazdu, id_planowy_czas_przyjazdu, id_planowy_czas_odjazdu, id_junk, id_kolejnosc_odcinkow, temperatura, roznica_czasu, ilosc_opadow) VALUES
+(1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 2.5, 0, 0),
+(1, 1, 2, 3, 1, 1, 1, 2, 3, 1, 2, 2.1, 5, 0),
+(1, 1, 3, 4, 1, 1, 1, 4, 5, 1, 3, 1.8, 0, 0),
+(1, 1, 4, 5, 1, 1, 1, 6, 7, 1, 4, 1.5, 0, 0),
+(2, 2, 1, 3, 2, 2, 2, 10, 11, 2, 1, 3.2, 0, 2),
+(2, 2, 3, 5, 2, 2, 2, 11, 12, 2, 2, 2.8, 8, 2),
+(2, 2, 5, 7, 2, 2, 2, 13, 14, 2, 3, 3.1, 0, 2),
+(2, 2, 7, 9, 2, 2, 2, 15, 16, 2, 4, 2.9, 0, 2),
+(3, 3, 2, 4, 3, 3, 3, 12, 13, 5, 1, 0.5, 0, 5),
+(3, 3, 4, 6, 3, 3, 3, 13, 14, 5, 2, 0.2, 10, 5),
+(3, 3, 6, 8, 3, 3, 3, 14, 15, 5, 3, -0.5, 0, 5),
+(3, 3, 8, 10, 3, 3, 3, 16, 17, 5, 4, -0.8, 0, 5);
 
--- Zdarzenie_na_trasie fact data
-INSERT INTO Zdarzenie_na_trasie (id_odcinek_kursu, id_przejazd, id_zdarzenie, id_junk, koszt_naprawy, predkosc, wywolane_opoznienie, liczba_rannych, liczba_zgonow) VALUES
-(1, 1, 1, 1, 15000.50, 85, 30, 2, 0),
-(1, 2, 2, 2, 8500.00, 75, 15, 0, 0),
-(2, 3, 3, 3, 0.00, 90, 5, 0, 0),
-(3, 4, 4, 4, 2500.75, 65, 10, 1, 0),
-(4, 5, 5, 5, 12000.00, 55, 45, 0, 1),
-(5, 1, 1, 1, 7800.25, 70, 20, 3, 0);
+INSERT INTO Zdarzenie_na_trasie (id_odcinek_kursu, id_przejazd, id_zdarzenie, id_junk, id_data_zdarzenia, id_czas_zdarzenia, koszt_naprawy, predkosc, wywolane_opoznienie, liczba_rannych, liczba_zgonow) VALUES
+(2, 1, 5, 1, 1, 9, 2500.50, 65, 5, 0, 0),
+(6, 2, 6, 2, 2, 12, 3200.00, 58, 3, 0, 0),
+(10, 3, 8, 1, 3, 16, 5600.75, 72, 5, 0, 0);
